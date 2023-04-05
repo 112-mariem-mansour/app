af49adb52ae11f52482e7bffb1397cc412815b8b
112-mariem-mansour <mansourmariemti12@gmail.com>
2023-04-05 11:03:46 +0100

    modif

--- App.js
+++ App.js
@@ -1,12 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, FlatList} from 'react-native';
+//import axios from 'axios';
+import { scheduleNotificationAsync } from 'expo-notifications';
+
+//const API_URL = 'http://127.0.0.1:8000/api/ordonnances';
 
 export default function App() {
+  const scheduleFirebaseNotification = async (date, time) => {
+    const trigger = new Date(Date.parse(`${date}T${time}:00.000Z`)).getTime();
+  
+    await scheduleNotificationAsync({
+      content: {
+        title: 'Titre de la notification',
+        body: 'Corps de la notification',
+        data: { someData: 'information' },
+      },
+      trigger,
+    });
+  };
+  scheduleFirebaseNotification('2023-04-04', '11:25');
+  /*const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    axios.get(API_URL)
+      .then((response) => {
+        setPosts(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+  <StatusBar style="auto" />
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <Text>{item.title}</Text>
+        )}
+      />   */
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+       
     </View>
+ 
   );
 }
 
